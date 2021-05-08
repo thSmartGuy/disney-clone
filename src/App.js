f8f73f79e935ffda1dd146e94013e3ae61fed5e8
Shubham Pandey <shubhampandey1097@gmail.com>
2021-05-08 23:33:43 +0530

    Front end is ready.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Details from './components/Details';
+import Login from './components/Login';
 import {
   BrowserRouter as Router,
   Switch, 
@@ -16,6 +17,9 @@ function App() {
       <Router>
         <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/detail">
             <Details />
           </Route>
